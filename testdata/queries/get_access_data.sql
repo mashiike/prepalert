878e2e5ef5e957d9ab196ee59f177a4a2792ca8c
mashiike <ikeda-masashi@kayac.com>
2022-09-07 19:38:23 +0900

    delete old config

--- testdata/queries/get_access_data.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    path, count(*) as cnt
-FROM access_log
-WHERE access_at
-    BETWEEN 'epoch'::TIMESTAMP + interval '{{ .Alert.OpenedAt }} seconds'
-    AND 'epoch'::TIMESTAMP + interval '{{ .Alert.ClosedAt }} seconds'
-GROUP BY 1
