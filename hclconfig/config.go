11c1c401ff7ab6339afde19e80fdf2f13b6ea0fe
mashiike <ikeda-masashi@kayac.com>
2022-10-14 16:23:19 +0900

    change interface

--- hclconfig/config.go
+++ hclconfig/config.go
@@ -33,7 +33,8 @@ func (cfg *Config) DecodeBody(body hcl.Body, ctx *hcl.EvalContext) hcl.Diagnosti
 	content, contentDiags := body.Content(schema)
 	diags = append(diags, contentDiags...)
 	diags = append(diags, hclconfig.RestrictOnlyOneBlock(content, "prepalert")...)
-	diags = append(diags, hclconfig.RestrictUniqueBlockLabels(content)...)
+	diags = append(diags, hclconfig.RestrictRequiredBlock(content, "prepalert")...)
+	diags = append(diags, hclconfig.RestrictUniqueBlockLabels(content, "rule")...)
 
 	for _, block := range content.Blocks {
 		switch block.Type {
